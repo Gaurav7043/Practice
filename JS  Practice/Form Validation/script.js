0a133dc781ccacd7ca1da8180fdcd7098700a950
Gaurav Kumar Gupta <128908032+Gaurav7043@users.noreply.github.com>
2023-07-23 22:46:58 +0530

    Add files via upload

--- JS  Practice/Form Validation/script.js
+++ JS  Practice/Form Validation/script.js
@@ -1,12 +1,15 @@
 // click event
-function click_fn(){
+function click_form(){
+    // debugger
     let f_name = document.getElementById("f_name").value;
     let m_name = document.getElementById("m_name").value;
     let l_name = document.getElementById("l_name").value;
-    let ress = document.getElementById("add").value;
+    let add = document.getElementById("add").value;
     let dob = document.getElementById("dob").value;
+    let male = document.getElementById("male").checked;
+    let female = document.getElementById("female").checked;
     if(f_name == "" || f_name == null){
-        document.getElementById("msg_1").innerText ="Enter First Name";
+        document.getElementById("msg_1").innerText = "Enter First Name";
         return false;
     }else if(m_name == "" || m_name == null){
         document.getElementById("msg_2").innerText = "Enter Middle Name";
@@ -14,11 +17,14 @@ function click_fn(){
     }else if(l_name == "" || l_name == null){
         document.getElementById("msg_3").innerText = "Enter Last Name";
         return false;
-    }else if(ress == "" || ress == null){
+    }else if(add == "" || add == null){
         document.getElementById("msg_4").innerText = "Enter Address";
         return false;
     }else if(dob == "" || dob == null){
-        document.getElementById("msg_5").innerText = "Enter Date";
+        document.getElementById("msg_5").innerText = "Select Date";
+        return false;
+    }else  if(male == false && female == false){
+        document.getElementById("msg_6").innerText = "Select Gender";
         return false;
     }
     else{
@@ -27,32 +33,25 @@ function click_fn(){
         document.getElementById("msg_3").innerText = "";
         document.getElementById("msg_4").innerText = "";
         document.getElementById("msg_5").innerText = "";
-    }
-
-    // radio click event
-    let male = document.getElementById("male").checked;
-    let female = document.getElementById("female").checked;
-    if(male == false && female == false){
-        document.getElementById("msg_6").innerText = "Select Gender";
-        return false;
-    }else{
         document.getElementById("msg_6").innerText = "";
+        // return false;
     }
 
     // select click event
-    let ct = document.getElementById("ct").value;
-    if(ct == "-1"){
-        document.getElementById("msg_7").innerText = "Select City";
+    let exp = document.getElementById("exp").value;
+    if(exp == "-1"){
+        document.getElementById("msg_7").innerText = "Select Experiance";
         return false;
     }else{
         document.getElementById("msg_7").innerText = "";
+        // return false;
     }
 
     // mobile click event
-    let mob = document.getElementById("mob").value;
-    if(mob == "" || mob == null){
-        document.getElementById("msg_8").innerText = "Enter Mobile No.";
-        return false; 
+    let mno = document.getElementById("mno").value;
+    if(mno == "" || mno == null){
+        document.getElementById("msg_8").innerText = "Enter Mobile No";
+        return false;
     }else{
         document.getElementById("msg_8").innerText = "";
     }
@@ -60,7 +59,7 @@ function click_fn(){
     // email click event
     let mail = document.getElementById("mail").value;
     if(mail == "" || mail == null){
-        document.getElementById("msg_9").innerText = "Enter mail";
+        document.getElementById("msg_9").innerText = "Enter Mail";
         return false;
     }else{
         document.getElementById("msg_9").innerText = "";
@@ -75,21 +74,30 @@ function click_fn(){
         document.getElementById("msg_10").innerText = "";
     }
 
-    // confirm password event
+    // confirm password click event
     let c_pass = document.getElementById("c_pass").value;
     if(c_pass == "" || c_pass == null){
         document.getElementById("msg_11").innerText = "Enter Confirm Password";
         return false;
     }else{
-        document.getElementById("msg_11").innerText = "";
-        return false;
+        document.getElementById("c_pass").innerText = "";
     }
 }
 
-// onbuler event
-function click_blan(blnk, span_id){
-    if(blnk.value == "" || blnk.value == null || blnk.value == "-1"){
-        document.getElementById(span_id).innerText = "Please Fill This Field !!!";
+// blure event
+function check_blank(blnk, span_id){
+    if(blnk.value == "" || blnk.value == null || blnk.value == -1){
+        document.getElementById(span_id).innerText = "Please, Fill This Fields !!!";
+        return false;
+    }else{
+        document.getElementById(span_id).innerText = ""
+        return false
+    }
+}
+
+function select_radio(blnk, span_id){
+    if(blnk.checked == false && blnk.checked == false){
+        document.getElementById(span_id).innerText = "Please, Fill This Fields !!!";
         return false;
     }else{
         document.getElementById(span_id).innerText = "";
@@ -97,22 +105,12 @@ function click_blan(blnk, span_id){
     }
 }
 
-function gend_check(blnk, span_id){
-    if(blnk.checked == false){
-        document.getElementById(span_id).innerText = "Please Fill This Field !!!";
-        return false;
-    }else{
-        document.getElementById(span_id).innerText = "";
-        return false;
-    }
-}
-
-// keybord event
+// =============keybord event==============
 // name validation
-function name_fn(blnk,span_id){
-    let regex = /^[a-zA-Z]*$/;
-    if(!(regex.test(blnk.value)) || (blnk.value.length < 2)){
-        document.getElementById(span_id).innerText = "Enter Only Alphabat";
+function regex_name(inputex, span_id){
+    let regex = /^[a-zA-Z]+$/;
+    if(!(regex.test(inputex.value)) || (inputex.value.length < 2)){
+        document.getElementById(span_id).innerText = "Only Alphabets Allowed";
         return false;
     }else{
         document.getElementById(span_id).innerText = "";
@@ -121,35 +119,35 @@ function name_fn(blnk,span_id){
 }
 
 // address validation
- function adde_fn(){
+function add_fn(){
     let add = document.getElementById("add").value;
     if(add.length <= 50){
         document.getElementById("msg_4").innerText = "Address Must be More Than 50 Character";
         return false;
     }else{
-        document.getElementById("msg_4").innerText = "";
+        document.getElementById("msg_4").innerText = ""
         return false;
     }
- }
+}
 
-//  mobile no. validation
-function mob_fn(blnk,span_id){
-    let regex = /^[0-9]+$/;
+// mobile Validation
+function mobile_reg(mobileex, span_id){
+    let regx = /^[0-9]*$/;
     let start = /^[6-9]/;
-    if(!(regex.test(blnk.value) && start.test(blnk.value))){
-        document.getElementById(span_id).innerText = "Enter Valid Number";
+    if(!(regx.test(mobileex.value) && start.test(mobileex.value))){
+        document.getElementById(span_id).innerText = "Enter Proper Number";
         return false;
-    }else if(blnk.value.length < 10){
+    }else if(mobileex.value.length < 10){
         document.getElementById(span_id).innerText = "Enter 10 Digits";
         return false;
     }else{
-        document.getElementById(span_id).innerText = "";
+        document.getElementById("msg_8").innerText = "";
         return false;
     }
 }
 
 // email validation
-function mail_fn(){
+function email_reg(){
     let mail = document.getElementById("mail").value;
     let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{3})+$/;
     if(regex.test(mail)){
@@ -162,7 +160,7 @@ function mail_fn(){
 }
 
 // password validation
-function pass_fn(){
+function pass_check(){
     let pass = document.getElementById("pass").value;
     let regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
     if(regex.test(pass)){
@@ -170,48 +168,31 @@ function pass_fn(){
         return false;
     }else{
         document.getElementById("msg_10").innerText = "Password Must be 8 to 15 Character";
-        return false;
     }
 }
 
 // compare password validation
-function pass_compare(){
+function pass_compare() {
     let pass = document.getElementById("pass").value;
     let c_pass = document.getElementById("c_pass").value;
-    if(c_pass != pass){
+    if (c_pass != pass) {
         document.getElementById("msg_11").innerText = "Password and Confirm Password does not match";
         return false;
-    }else{
+    } else {
         document.getElementById("msg_11").innerText = "";
         return false;
     }
 }
 
 // show password validation
-function show_pass(){
+function show_pass() {
     let pass = document.getElementById("pass");
-    let icon_1 = document.getElementById("icon_1");
-    if(pass.type == "password" && icon_1.classList.contains("fa-eye") == true){
-        pass.type = "text";
-        icon_1.classList.remove("fa-eye");
-        icon_1.classList.add("fa-eye-slash");
-    }else{
-        pass.type = "password";
-        icon_1.classList.remove("fa-eye-slash");
-        icon_1.classList.add("fa-eye")
-    }
-}
-
-function c_pass(){
     let c_pass = document.getElementById("c_pass");
-    let icon_2 = document.getElementById("icon_2");
-    if(c_pass.type == "password" && icon_2.classList.contains("fa-eye") == true){
+    if ((pass.type == "password") && (c_pass.type == "password")) {
+        pass.type = "text";
         c_pass.type = "text";
-        icon_2.classList.remove("fa-eye");
-        icon_2.classList.add("fa-eye-slash");
-    }else{
+    } else {
+        pass.type = "password";
         c_pass.type = "password";
-        icon_2.classList.remove("fa-eye-slash");
-        icon_2.classList.add("fa-eye");
     }
 }
